3cccbea0e722667f059273faa14af2d70e4223c3
Arsene Mayam <mayamarsene@yahoo.com>
2017-11-25 15:51:12 -0500

    CommandeActivity

--- app/src/main/java/com/example/arsene/mamieclafoutisandroid/CommandeActivity.java
+++ app/src/main/java/com/example/arsene/mamieclafoutisandroid/CommandeActivity.java
@@ -4,13 +4,18 @@ import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ListView;
+
+import adapters.CommandeAdapter;
+import entities.Produit;
 
 public class CommandeActivity extends AppCompatActivity {
 
     Context ctx;
     ListView commandeListView;
+    CommandeAdapter adapter;
     Button passerCommande;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,10 +25,25 @@ public class CommandeActivity extends AppCompatActivity {
         passerCommande = (Button) findViewById(R.id.bttnPasserCommande);
         commandeListView = (ListView) findViewById(R.id.commandeListeView);
 
+        // initialisation commande adapter
+        //adapter = new CommandeAdapter(ctx,R.layout.commande_view,)
+
+
+        // listener sur la list view
+        commandeListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+                Produit produit =(Produit) adapterView.getItemAtPosition(i);
+
+
+            }
+        });
 
 
 
-        // listen bttn passer commande
+
+        // listener bttn passer commande
         passerCommande.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
