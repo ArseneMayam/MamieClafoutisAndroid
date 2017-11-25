ee0d5409628cfbf7e47943e3343e23b0164e027e
Arsene Mayam <mayamarsene@yahoo.com>
2017-11-25 16:28:01 -0500

    Boutique Activity

---
+++ app/src/main/java/com/example/arsene/mamieclafoutisandroid/BoutiqueActivity.java
@@ -0,0 +1,16 @@
+package com.example.arsene.mamieclafoutisandroid;
+
+import android.content.Context;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BoutiqueActivity extends AppCompatActivity {
+    Context ctx;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_boutique);
+        ctx = this;
+
+    }
+}
