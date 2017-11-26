254537a81b875e2a24a3a490d0a317b5ceff45e8
Unknown <brice.mimifir.a@gmail.com>
2017-11-26 11:12:20 -0500

    Add vente
    view of the vente

---
+++ app/src/main/java/com/example/arsene/mamieclafoutisandroid/vente.java
@@ -0,0 +1,13 @@
+package com.example.arsene.mamieclafoutisandroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class vente extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vente);
+    }
+}
