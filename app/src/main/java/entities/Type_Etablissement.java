d01af19adf41c15d6b73bd7de0e4bc30d297f930
Arsene Mayam <mayamarsene@yahoo.com>
2017-11-27 10:50:40 -0500

    Update entity : Type_Etablissement

--- app/src/main/java/entities/Type_Etablissement.java
+++ app/src/main/java/entities/Type_Etablissement.java
@@ -5,4 +5,33 @@ package entities;
  */
 
 public class Type_Etablissement {
+    private int id;
+    private String type;
+
+    // constructeur
+    public Type_Etablissement(int id, String type) {
+        this.id = id;
+        this.type = type;
+    }
+
+    public Type_Etablissement(int id) {
+        this.id = id;
+    }
+
+    //getters et setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
 }
